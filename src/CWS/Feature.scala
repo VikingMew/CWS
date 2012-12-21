8fb788c777d37ee3ab971d71fbeca439c5be42f4
vikingmew <vikingmew@vikingmew-T410.(none)>
2012-12-22 03:13:50 +0800

    feature generator Cx with list offset

--- src/CWS/Feature.scala
+++ src/CWS/Feature.scala
@@ -10,9 +10,19 @@ package CWS
 object Feature {
   def c(a:Array[Point2],index:Int,offset:Int) = {
     if(index+offset>=0 && index+offset < a.length)
-    (b:Array[Point2],index2:Int) =>
-      if(index+offset < b.length)
-        if(b(index2+offset).c ==  a(index+offset).c) 1 else 0
+      (b:Array[Point2],index2:Int) = {
+        if(index2+offset < b.length)
+          if(b(index2+offset).c ==  a(index+offset).c) 1 else 0
+      }
+    else
+      null
+  }
+  def c(a:Array[Point2],index:Int,offset:List[Int]) = {
+    if(index+offset.min >=0 && index+offset.max < a.length)
+      (b:Array[Point2],index2:Int) = {
+        if(index2+offset.max < b.length)
+           offset.map(x =>c(b,index2,x)).reduce(*)
+      }
     else
       null
   }
