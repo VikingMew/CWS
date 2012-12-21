2477ee629803b29c02a5e5f72632cf1c31a22863
vikingmew <vikingmew@vikingmew-T410.(none)>
2012-12-22 03:06:32 +0800

    feature generator Cx

--- src/CWS/Feature.scala
+++ src/CWS/Feature.scala
@@ -11,7 +11,7 @@ object Feature {
   def c(a:Array[Point2],index:Int,offset:Int) = {
     if(index+offset>=0 && index+offset < a.length)
     (b:Array[Point2],index2:Int) =>
-      if(index+offset>=0 && index+offset < b.length)
+      if(index+offset < b.length)
         if(b(index2+offset).c ==  a(index+offset).c) 1 else 0
     else
       null
